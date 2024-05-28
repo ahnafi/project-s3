361e93f7806f2135f21fab849fc67acb9bf4b189
Eko Susilo <ekosusilo140604@gmail.com>
2024-05-28 19:31:31 +0700

    feat: add hero section of home

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        body: ['Poppins', ...defaultTheme.fontFamily.sans],
+        heading: ['Orelega One', ...defaultTheme.fontFamily.sans],
+        jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "yellow-base": "#FFAA19",
+        "red-base": "#961522",
+        "green-base": "#2F9D70",
+        "yellow-light": "#E8DCA8",
+        "red-light": "#FF907D",
+        "green-light": "#7CC782"
+      }
+    },
+  },
+  plugins: [],
+};
