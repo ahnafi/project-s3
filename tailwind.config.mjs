60e577e737bd6a0b07d3fdc8db187c5126f720f1
jackberck <berckjack@gmail.com>
2024-05-31 14:15:08 +0700

    chore & fix: adding butterfly logo to navbar and fixing navbar and footer section display

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-        body: ['Poppins', ...defaultTheme.fontFamily.sans],
-        heading: ['Orelega One', ...defaultTheme.fontFamily.sans],
-        jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        body: ["Poppins", ...defaultTheme.fontFamily.sans],
+        heading: ["Orelega One", ...defaultTheme.fontFamily.sans],
+        jakarta: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         "yellow-base": "#FFAA19",
@@ -17,8 +17,13 @@ export default {
         "green-base": "#2F9D70",
         "yellow-light": "#E8DCA8",
         "red-light": "#FF907D",
-        "green-light": "#7CC782"
-      }
+        "green-light": "#7CC782",
+        "white-base": "#f1f5f9",
+        "dark-base": "#0f172a",
+      },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+      },
     },
   },
   plugins: [],
