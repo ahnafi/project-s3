b5c5a20b0e4bb3618e98f6399ba038e481c1a6b6
ahnafi <devsulthon@gmail.com>
2024-06-22 06:47:02 +0700

    feat (ukm pages): add ukm pages and description & card

---
+++ src/data/tentang-unsoed.ts
@@ -0,0 +1,131 @@
+export interface DataUkm {
+  namaUkm: string;
+  kategori?: string;
+  deskripsi: string;
+  kegiatan: string[];
+  photoProfile: string;
+  galery?: string[];
+  prestasi?: string[];
+}
+
+const ukm: DataUkm[] = [
+  {
+    namaUkm: "robotic",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+    galery:["/images/cartoon/1.jpg","/images/cartoon/2.jpg","/images/cartoon/3.jpg","/images/cartoon/4.jpg"]
+  },
+  {
+    namaUkm: "catur",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+  {
+    namaUkm: "ukki",
+    deskripsi:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit veniam aperiam ab velit dolores molestias consequuntur explicabo at est magni ea dolorum molestiae sunt sequi voluptatem ut amet fuga alias.",
+    kegiatan: [
+      "kegiatan 1",
+      "kegiatan 2",
+      "kegiatan 3",
+      "kegiatan 4",
+      "kegiatan 5",
+    ],
+    photoProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMEjPLjHnMburMWJrIIwxQLihDzLAyTM_YMQ&s",
+  },
+];
+
+const ormawa = [];
+
+const paguyuban = [];
+
+export { ukm, ormawa, paguyuban };
