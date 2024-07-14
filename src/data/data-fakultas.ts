a9cbee1168bdac09adb7ef4f0e1c82f1ca1ca039
jackberck <berckjack@gmail.com>
2024-07-14 20:46:05 +0700

    docs: changing logo each faculty and removing some unecessary images

--- src/data/data-fakultas.ts
+++ src/data/data-fakultas.ts
@@ -4,63 +4,65 @@ const dataFakultas: RagamInfoItem[] = [
   {
     title: "Fakultas Pertanian",
     desc: `Fakultas Pertanian Universitas Jenderal Soedirman merupakan fakultas tertua dengan Program Studi terbanyak di Unsoed. Fakultas Pertanian Unsoed resmi didirikan pada tahun 1963.  Program studi di Fakultas Pertanian, diantaranya: Agribisnis (D3), Perencanaan Sumber Daya Lahan (D3), Agribisnis (S1), Teknik Pertanian (S1), Agroteknologi (S1), Teknologi Pangan (S1), Proteksi Tanaman (S1),  Agronomi (S2), Agribisnis (S2), Ilmu Pangan (S2), Ilmu Pertanian (S3). `,
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor: "bg-[#046941]",
   },
   {
     title: "Fakultas Biologi",
     desc: `Fakultas Biologi Unsoed berdiri bersamaan dengan saat berdirinya Unsoed itu sendiri. Fakultas Biologi Universitas Jenderal Soedirman berdiri pada tanggal 23 November 1963. Pada awalnya, fakultas ini terdiri atas tiga jurusan, yaitu Botani, Zoologi, dan Biologi Umum. Pada saat ini Fakultas Biologi memiliki 1 program studi D3, 3 program studi S1, 1 program studi S2, dan 1 program studi S3. `,
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor: "bg-[#0101FA]",
   },
   {
     title: "Fakultas Ekonomi dan Bisnis",
     desc: `Fakultas Ekonomi dan Bisnis Universitas Jenderal Soedirman didirikan pada tanggal 9 Oktober 1963 dan Fakultas ini merupakan satu dari tiga fakultas pertama, yakni bersama Fakultas Pertanian dan Fakultas Biologi yang merupakan embrio dari lahirnya Universitas Jenderal Soedirman pada tahun 1963. Fakultas ini memiliki 4 program studi D3 dan 7 program studi S1. `,
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor: "bg-[#9F9E9D]",
   },
   {
     title: "Fakultas Peternakan",
     desc: "Fakultas Peternakan Universitas Jenderal Soedirman diresmikan tanggal 10 Februari 1966 dengan fokus pengembangan sumberdaya peternakan dan kearifan lokal secara berkelanjutan, melalui penguasaan sains produksi, nutrisi dan pakan, sosial ekonomi serta teknologi peternakan. Fakultas Peternakan membina empat program studi yaitu: Budidaya Ternak (D3), Peternakan (S1), Peternakan (S2), Peternakan (S3)",
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor: "bg-[#FF9934]",
   },
   {
     title: "Fakultas Hukum",
     desc: `Pendirian Fakultas Hukum Unsoed didahului
     dengan pembentukan panitia berdasarkan Keputusan Rektor Universitas Jenderal Soedirman melalui Surat Kep. 022/PT30.Y/E.1979. Jenderal Soedirman dapat mulai menerima mahasiswa tahun akademik 1981/1982. Fakultas Hukum terdiri atas dua program studi:  Hukum (S1) dan Hukum Internasional (S1) `,
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor: "bg-[#FC0101]",
   },
   {
     title: "Fakultas Ilmu Sosial dan Ilmu Politik",
     desc: "Fakultas Ilmu Sosial dan Ilmu Politik Universitas Jenderal Soedirman berdiri pada tahun 1993 disertai dengan dibukanya Program Studi Sosiologi dan Ilmu Administrasi Negara. FISIP UNSOED memiliki empat jurnal publikasi yaitu Acta Diurna, Insignia, Widya Komunika, dan Public Policy. Saat ini Fakultas ISIP terdiri atas 6 program studi (S1), 4 program studi (S2), dan 1 program studi (S3).",
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor: "bg-[#FF6502]",
   },
   {
     title: "Fakultas Kedokteran",
     desc: `Fakultas Kedokteran Universitas Jenderal Soedirman berdiri pada tahun 2014 berdasarkan permendikbud No 21 Tahun 2014.Fakultas Kedokteran memiliki beberapa kampus yaitu Kampus Gumbreg untuk Jurusan Pendidikan Kedokteran, Kampus Karangwangkal dan gedung Integrated Academic Building untuk Jurusan Pendidikan Kedokteran Gigi. Profesi Kedokteran Bertempat pada Rumah Sakit Margono dan Rumah Sakit Jejaring yang lain. Fakultas Kedokteran memiliki 2 program studi (S1), 1 program studi (S2), dan 3 program profesi serta pendidikan.`,
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor: "bg-[#3CB372]",
   },
   {
     title: "Fakultas Teknik",
     desc: "Fakultas Teknik Universitas Jenderal Soedirman resmi didirikan pada tanggal 24 Oktober 2014. Fakultas Teknik terletak di Kampus Unsoed Purbalingga, Jawa Tengah. Fakultas Teknik Universitas Jenderal Soedirman merupakan salah satu fakultas yang memiliki banyak mahasiswa berprestasi dan  berkompeten baik dibidang akademik maupun non akademik. Untuk mendukung kompetensi yang dimiliki oleh mahasiswa, Fakultas Teknik terdiri atas 7 Program studi (S1) dan 1 program studi (S2). ",
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor: "bg-[#14076F]",
   },
   {
     title: "Fakultas Ilmu-ilmu Kesehatan",
     desc: "Fakultas Ilmu-ilmu Kesehatan Universitas Jenderal Soedirman merupakan salah satu Fakultas yang menyelenggarakan pendidikan akademik tingkat sarjana (S1) dan profesi dalam seperangkat cabang ilmu-ilmu kesehatan. Fakultas Ilmu-ilmu Kesehatan mulai berdiri sejak tahun 2014. Fakultas Ilmu-ilmu Kesehatan saat ini memiliki 6 program studi (S1), 2 program studi (S2), dan 2 Program profesi.",
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor: "bg-[#3DFEFE]",
   },
   {
     title: "Fakultas Ilmu Budaya",
     desc: "Fakultas Ilmu Budaya Universitas Jenderal Soedirman berdiri pada tahun 2014. Sebelum Fakultas Ilmu Budaya berdiri, jurusan Ilmu Budaya sudah hadir di Unsoed dan masuk ke dalam Fakultas Ilmu-ilmu Sosial Politik, namun seiring berjalannya waktu peminat jurusan Ilmu Budaya semakin meningkat sehingga berdirilah Fakultas Ilmu Budaya. FIB UNSOED memiliki 2 program studi (D3) dan 6 program studi (S1).",
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor: "bg-[#FFFFFF]",
   },
   {
     title: "Fakultas Matematika dan Ilmu Pengetahuan Alam",
     desc: "Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Jenderal Soedirman berdiri sejak tahun 2014. Pada tahun 2014, Jurusan Matematika dan Ilmu Pengetahuan Alam memisahkan diri dari Fakultas Sains dan Teknik, kemudian membentuk fakultas independen yang bernama Fakultas Matematika dan Ilmu Pengetahuan Alam. Saat ini FMIPA memiliki 4 program studi (S1). ",
-    img: "https://www.bem.ft.unsoed.ac.id/wp-content/uploads/2023/09/Logo-BEM-FT.png",
+    facultyColor:
+      "bg-gradient-to-r from-[#800202] from-35% via-[#FFD303] via-50% to-[#800202] to-65%",
   },
   {
     title: "Fakultas Perikanan dan Ilmu Kelautan",
     desc: "Fakultas Perikanan dan Ilmu Kelautan Universitas Jenderal Soedirman didirikan berdasarkan Keputusan Mendiknas dan SK Rektor Unsoed pada tahun 2014 Nomor 1600/UN23/OT.01/2014 tanggal 24 Oktober 2014 tentang Penetapan Fakultas-Fakultas baru Universitas Jenderal Soedirman. FPIK UNSOED memiliki 3 program studi (S1) dan 1 program studi (S2).",
-    img: "https://fmipa.unsoed.ac.id/v4/wp-content/uploads/logo-bem-km-mipa-1024x1024.png",
+    facultyColor:
+      "bg-gradient-to-br from-[#000066] from-35% via-[#02FFFF] via-50% to-[#000066] to-65%",
   },
 ];
 
