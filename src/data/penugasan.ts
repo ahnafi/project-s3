877d94c0b8752bc79999faa04594dd43104ee512
jackberck <berckjack@gmail.com>
2024-07-25 20:48:41 +0700

    refactor: changing path of each penugasan description

--- src/data/penugasan.ts
+++ src/data/penugasan.ts
@@ -1,62 +1,102 @@
-interface Penugasan {
-  nomor?: string;
-  img: string;
-  title: string;
-  tugas?: string[];
-}
+const penugasan = {
+  penugasanPertama: {
+    title: "Penugasan Pertama",
+    image: "/images/butterfly-metamorphosis/telur.webp",
+    description: [
+      {
+        type: "Borang FGD",
+        task: [
+          "Penugasan borang FGD akan dipublikasikan melalui media sosial Instagram masing masing.",
+          "Penugasan borang FGD diupload melalui Instastory setelah pertemuan persesi selesai.",
+        ],
+      },
+      {
+        type: "My Dream Canvas",
+        task: [
+          "Membuat video singkat yang berisi perkenalan diri serta harapan selama berkuliah hingga karir yang diinginkan.",
+          "Diharapkan mahasiswa baru dapat memimplementasi nilai “Self Discovery” dan “Self Actualization”.",
+        ],
+      },
+      {
+        type: "Branding SOLO",
+        task: [
+          "Branding SOLO bertujuan untuk meningkatkan kesadaran akan kepemimpinan khususnya untuk mahasiswa baru.",
+          "Penugasan ini menjadi alternatif untuk memberi gambaran yang mudah dipahami terkait gaya kepemimpinan kepada mahasiswa baru di era society 5.0.",
+        ],
+      },
+      {
+        type: "Mental Health Campaign",
+        task: [
+          "Mindful Moment merupakan bentuk mental health campaign.",
+          "Mahasiswa diharapkan dapat mengalami peningkatan dalam kesejahteraan emosional mereka.",
+        ],
+      },
+    ],
+  },
+  penugasanKedua: {
+    title: "Penugasan Kedua",
+    image: "/images/butterfly-metamorphosis/ulat.webp",
+    description: [
+      {
+        type: "Linkedin",
+        task: [
+          "Membuat akun Linkedin dengan ketentuan fitur yang harus diisi.",
+          "Pengumpulan tugas berupa link dan screenshot Linkedin dikirim melalui Google Form yang telah disediakan.",
+        ],
+      },
+      {
+        type: "Video Kelompok",
+        task: [
+          'Membuat video berbentuk mini talkshow dengan tema "Navigating Your Career Path. From Passion to Profession" dan sesuai ketentuan yang berlaku.',
+          "Penugasan ini melatih Gensoed dalam kreativitas dan inovasi dalam menyelesaikan masalah.",
+        ],
+      },
+      {
+        type: "Papermob dan Papan Kelompok",
+        task: [
+          "Merancang papermob sesuai dengan bagiannya masing-masing.",
+          "Membuat papan kelompok yang berisikan nama kelompok dan gugus, serta ketentuan yang sudah ditentukan.",
+        ],
+      },
+      {
+        type: "Borang FGD",
+        task: [
+          "Penugasan borang FGD akan dipublikasikan melalui media sosial Instagram masing masing.",
+          "Penugasan borang FGD diupload melalui Instastory setelah pertemuan persesi selesai.",
+        ],
+      },
+    ],
+  },
+  penugasanKetiga: {
+    title: "Penugasan Ketiga",
+    image: "/images/butterfly-metamorphosis/kepompong.webp",
+    description: [
+      {
+        type: "Oxyvit",
+        task: [
+          "Gensoed diwajibkan membeli bundle Oxyvit pada pendamping masing-masing sesuai jadwal pemesanan.",
+          "Gensoed ditugaskan untuk menulis konten promosi berupa video berbentuk foto slide dengan tujuan mempengaruhi target sesuai dengan ketentuan.",
+          "Video diupload di Tiktok dan dikumpulkan melalui Google Form yang telah disediakan (berupa link Tiktok).",
+        ],
+      },
+      {
+        type: "Action Plan",
+        task: [
+          "Menyusun rencana tindakan yang jelas dan terukur untuk mencapai tujuan yang telah ditetapkan.",
+          "Penugasan diupload di Feeds Instagram masing-masing sesuai dengan ketentuan yang berlaku.",
+          "Tugas dikumpulkan berupa link dan screenshot Instagram yang telah diupload melalui Google Form.",
+        ],
+      },
+      {
+        type: "Twibbon, Yel-yel, dan Borang FGD",
+        task: [
+          "Twibbon dengan foto diri yang paling menarik sesuai dengan template yang sudah disediakan dan diupload melalui Feeds Instagram.",
+          "Membuat yel-yel yang kreatif dan inovatif sesuai dengan ketentuan yang berlaku dan dapan digunakan sebagai penyemangat.",
+          "Penugasan borang FGD akan dipublikasikan melalui media sosial Instagram masing-masing dan diupload melalui Instastory setelah pertemuan persesi selesai.",
+        ],
+      },
+    ],
+  },
+};
 
-const snbpSnbt: Penugasan[] = [
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "1",
-    tugas: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nisi suscipit officia sunt repellat, consequatur et quisquam. Culpa voluptatum sit dolore cum tempora accusantium! Nesciunt quo dolorem consectetur ex corporis?", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "2",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "3",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "4",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-];
-
-const mandiri: Penugasan[] = [
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "1",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "1",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "1",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-  {
-    img: "/images/cartoon/1.jpg",
-    title: "Telur",
-    nomor: "1",
-    tugas: ["lorem", "ipsum", "dolor"],
-  },
-];
-
-export { type Penugasan, mandiri, snbpSnbt };
+export { penugasan };
